85599e3214d14be8a17f83f3376ccff505c37b74
ratinfx <ratina0@gmail.com>
2024-01-07 19:14:00 +0100

    Added const General, Extension, and File strings

---
+++ VPExtensionManager.Core/Models/RFXStrings.cs
@@ -0,0 +1,22 @@
+﻿namespace VPExtensionManager.Core.Models
+{
+    public class RFXStrings
+    {
+        // General
+        public const string RatinFX = "RatinFX";
+
+        // Extensions
+        public const string VPConsole = "VPConsole";
+        public const string VegasProFlow = "VegasProFlow";
+        public const string ShortenExtendMedia = "ShortenExtendMedia";
+        public const string CustomFades = "CustomFades";
+
+        // References
+        public const string VegasProData = "VegasProData";
+        public const string RatinFXVP = "RatinFX.VP";
+        public const string NewtonsoftJson = "Newtonsoft.Json";
+
+        public static string[] VPConsoleRefs = [VegasProData, RatinFXVP, NewtonsoftJson];
+        public static string[] VegasProFlowRefs = [VegasProData, RatinFXVP, NewtonsoftJson];
+    }
+}
