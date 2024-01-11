303b83ec0e200c5e441e639e6ee072f68680fc40
ratinfx <ratina0@gmail.com>
2024-01-11 20:24:58 +0100

    Use RFXString instead for duplicate string value

--- VPExtensionManager.Core/Services/GitHubService.cs
+++ VPExtensionManager.Core/Services/GitHubService.cs
@@ -1,14 +1,15 @@
 ﻿using Octokit;
+using VPExtensionManager.Core.Models;
 
 namespace VPExtensionManager.Core.Services;
 
 public class GitHubService
 {
     public static GitHubClient Client { get; private set; } = new GitHubClient(new ProductHeaderValue("VPExtensionManager_" + DateTime.Now.Ticks));
-    public static string GitHubUserName => "RatinFX";
+    public static string GitHubUserName => RFXStrings.RatinFX;
 
     public static Release GetLatestRelease(string extensionName)
     {
-        return Client.Repository.Release.GetLatest(GitHubUserName, extensionName).Result;
+        return Client.Repository.Release.GetLatest(GitHubUserName, extensionName)?.Result;
     }
 }
