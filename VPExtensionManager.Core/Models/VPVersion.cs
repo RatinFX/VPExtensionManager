0d2e7b2b3ea5032aeccb307ece06c29ca8fe49d3
ratinfx <ratina0@gmail.com>
2024-01-14 01:45:59 +0100

    Added Enum Descriptions to display on Install

--- VPExtensionManager.Core/Models/VPVersion.cs
+++ VPExtensionManager.Core/Models/VPVersion.cs
@@ -1,8 +1,12 @@
-﻿namespace VPExtensionManager.Core.Models;
+﻿using System.ComponentModel;
+
+namespace VPExtensionManager.Core.Models;
 
 public enum VPVersion
 {
     Unknown = 0,
+    [Description("For VP13 and below")]
     Sony = 13,
+    [Description("For VP14 and above")]
     Magix = 14
 }
---
+++ VPExtensionManager/Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace VPExtensionManager.Converters;
+
+public class EnumDescriptionConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null || !value.GetType().IsEnum)
+            return value;
+
+        var enumValue = (Enum)value;
+        var field = enumValue.GetType().GetField(enumValue.ToString());
+
+        if (field == null)
+            return value;
+
+        var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+
+        var result = attribute != null
+            ? $"{attribute.Description} ({value})"
+            : value.ToString();
+
+        return result;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
