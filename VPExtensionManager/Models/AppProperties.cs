4080d34868278fc7352af26b5043f73b4f619da9
ratinfx <ratina0@gmail.com>
2024-02-01 00:58:59 +0100

    Added summary for AppProperties consts

--- VPExtensionManager/Models/AppProperties.cs
+++ VPExtensionManager/Models/AppProperties.cs
@@ -2,9 +2,21 @@
 
 public class AppProperties
 {
+    /// <summary>
+    /// The selected app Theme
+    /// </summary>
     public const string Theme = "Theme";
+    /// <summary>
+    /// A list of installed VP versions
+    /// </summary>
     public const string VPVersions = "VPVersions";
+    /// <summary>
+    /// Whether an install should be re-downloaded or not
+    /// </summary>
     public const string ForceDownload = "ForceDownload";
+    /// <summary>
+    /// The folder where the GitHub releases should be downloaded to
+    /// </summary>
     public const string DownloadsFolder = "DownloadsFolder";
 
     public static bool Contains(string key)
