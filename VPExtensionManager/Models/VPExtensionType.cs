733ae85c9c5a732993e389be72b1a3bdd75f83b2
ratinfx <ratina0@gmail.com>
2024-03-07 14:20:18 +0100

    Removed FileExtension from VPExtensionType

--- VPExtensionManager/Models/VPExtensionType.cs
+++ VPExtensionManager/Models/VPExtensionType.cs
@@ -1,13 +1,12 @@
 ﻿namespace VPExtensionManager.Models;
 
-public class VPExtensionType(string name, string fileExtension, string downloadExtension)
+public class VPExtensionType(string name, string downloadExtension)
 {
     public string Name { get; set; } = name;
-    public string FileExtension { get; set; } = fileExtension;
     public string DownloadFileExtension { get; set; } = downloadExtension;
 
-    public static readonly VPExtensionType Extension = new("Extension", RFXStrings.Dll, RFXStrings.Zip);
-    public static readonly VPExtensionType Script = new("Script", RFXStrings.Dll, RFXStrings.Dll);
+    public static readonly VPExtensionType Extension = new("Extension", RFXStrings.Zip);
+    public static readonly VPExtensionType Script = new("Script", RFXStrings.Dll);
 
     public bool Equals(VPExtensionType other) => Name == other.Name;
 }
